e8b6bb9123f46bae1333e33fe3d3322dc4f03fad
Unknown <zerpa2007@hotmail.com>
2021-07-24 11:59:19 -0400

    Added Create, Update and Delete Product
    Not tested yet

---
+++ backend/src/helpers/auth.product.js
@@ -0,0 +1,76 @@
+const db = require('../utils/db');
+const queries = require('../utils/queries');
+
+const createProduct = async (req, res) => {
+    const {name, dsc, price} = req.body;
+
+    try {
+    
+    await db.query('BEGIN');
+    
+    const res = await db.query(queries.CREATE_, [name, dsc, price]);
+    console.log('Product Created' + res.rows);
+    await db.query('COMMIT');
+    res.status(200).send(`Product registration (${name}) successfull!`);
+    
+    
+    }catch(err) {
+        res.status(500).send('Server Error! ');
+        throw err;
+    }
+}
+
+const updateProduct = async (req, res) => {
+    const id = req.params.id;
+
+    const {name, dsc, price} = req.body;
+    console.log(name, dsc, price);
+
+    try{
+       await db.query('BEGIN'); 
+       const checkId = await db.query(queries.CHECKPRODUCTID, [id]);
+        
+       if(checkId.rows != ''){
+        const response = await db.query(queries.UPDATE_PRODUCT, [name, dsc, price, id]);
+        console.log(response.rows);
+        res.status(200).send(`Product ${id} updated!`)
+        await db.query('COMMIT');
+       }else{
+        console.log(checkId);
+        await db.query('ROLLBACK');
+        res.status(400).send(`Product ${id} not Found!`);
+       }
+    }catch(err){
+        await db.query('ROLLBACK');
+        res.status(500).send('Server Error!');
+        throw err;
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try{
+       const id = req.params.id;
+        
+       await db.query('BEGIN'); 
+       const checkId = await db.query(queries.CHECKProductID, [id]);
+
+       if(checkId != '') {
+           const response = await db.query(queries.DELETE_Product, [id]);
+           console.log(response.rows);
+           res.status(200).send(`Product ${id} deleted!`);
+            await db.query('COMMIT');
+       }else{
+        await db.query('ROLLBACK');
+        res.status(400).send(`Product ${id} not Found!`);
+       }
+    }catch(err){      
+        await db.query('ROLLBACK');
+        res.status(500).send('Server Error!');
+        throw err;
+    }
+}
+module.exports = {
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
--- backend/src/utils/queries.js
+++ backend/src/utils/queries.js
@@ -17,15 +17,24 @@ const querys = {
     CHECKUSER: `SELECT * FROM public."Users" WHERE username = $1`,
     CHECKID: `SELECT * FROM public. "Users" WHERE id = $1`,
     CHECKEXISTENCE: `SELECT * FROM public. "Commerce" WHERE name = $1`,
-    CHECKCOMMERCEID: `SELECT * FROM public. "COmmerce" WHERE id = $1`,
+    CHECKCOMMERCEID: `SELECT * FROM public. "Commerce" WHERE id = $1`,
+    CHECKPRODUCTID: `SELECT FROM public. "Product" WHERE id = $1`,
 
     //Commerce Queries
 
     CREATE_COMME: `INSERT INTO public. "Commerce" (name, category, location, dsc) VALUES ($1, $2, $3, $4) RETURNING *`,       
     GET_COMMERCES: `SELECT * FROM public. "Commerce"`,
-    GET_COMMERCEBYID: `SELECT * FROM public. "Commerce" WHERE id =$1`,
-    UPDATE_COMMERCE: `UPDATE public. "Tasks" SET title = $1, dsc = $2, tag = $3, dt = $4 WHERE id = $5 RETURNING *`,
+    GET_COMMERCEBYID: `SELECT * FROM public. "Commerce" WHERE id = $1`,
+    UPDATE_COMMERCE: `UPDATE public. "Tasks" SET name = $1, category = $2, location = $3, dsc = $4 WHERE id = $5 RETURNING *`,
     DELETE_COMMERCE: `DELETE FROM public. "Tasks" WHERE id = $1`,
+
+    //Prodduct Queries
+
+    CREATE_PRODUCT: `INSERT INTO public. "Product" (name, dsc, price) VALUES ($1, $2, $3) RETURNING *`,
+    UPDATE_PRODUCT: `UPDATE public. "Product" SET name = $1, dsc = $2, price = $3 WHERE id = $5`,
+    DELETE_PRODUCT: `DELETE FROM public. "Product" WHERE id = $1`,
+    GET_PRODUCTS: `SELECT * FROM public. "Products"`,
+    GET_PRODUCTSBYCOMMERCE: `SELECT * FROM public. "Products" WHERE idcommerce = $1`
 }
 
 module.exports = querys;
